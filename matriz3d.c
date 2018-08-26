072aa37fc1664e51ac718e4bff683603c27747e2
Mateus Assis <mateus.d.assis.silva@gmail.com>
2018-08-26 19:25:14 -0300

    Atualização na matriz 3d

--- matriz3d.c
+++ matriz3d.c
@@ -42,6 +42,8 @@ int main(){
     x[0][0] = (int *)malloc(sizeof(int)*nf*nc*nl);
     y[0][0] = (int *)malloc(sizeof(int)*nf*nc*nl);
 
+
+
     if(x[0][0] == NULL || y[0][0] == NULL){
         printf("erro ao alocar vetor que tem papel de matriz \n");
         exit(0);
@@ -49,23 +51,30 @@ int main(){
 
 
     for ( j = 0; j<=nc; j++){
-        for(i = 1; i<= 1; i++){
+        printf("\n");
+        for(i = 0; i<= 1; i++){
             x[i][j] = x[0][0] + ((j+i*nl)*nc)*sizeof(int);
             y[i][j] = y[0][0] + ((j+i*nl)*nc)*sizeof(int);
-        }
-    }
-  printf("consegui alocar completamente a matriz");
-    /*
-    // aqui eu deveria preencher a matriz 3d em si.
-    for(k = 0 ; k < nf; k++){ printf("folha %d: \n", k);
-        for(j = 0; j < nc; j++){
-            for(i = 0; i < nl; i++){
-                x[i][j][k] = rand()%10;
-                printf("%d ",x[i][j][k]);
-            }printf("\n");
+            printf("x[%d][%d] = %p , ",i, j, x[i][j]);
+            if(x[i][j] == 0 ||y[i][j] == 0 ){
+                printf("Falha na alocação de memoria de x ou de y para a posicao [%d][%d]", i, j);
+                exit(0);
+}
         }
     }
 
+ // printf("consegui alocar completamente a matriz");
+/*
+    // aqui eu deveria preencher a matriz 3d em si.
+    for(i = 0 ; i < nl; i++){ //printf("folha %d: \n", k);
+        for(j = 0; j < nc; j++){
+            for(k = 0; k < nf; k++){
+                x[i][j][k] = 1;
+                printf("%d ",x[i][j][k]);
+            }//printf("\n");
+        }
+    }
 */
+
     return 0;
 }
