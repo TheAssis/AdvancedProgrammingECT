bd277ca0dbe58c990a03951f0dafc25114ba985a
Mateus Assis <mateus.d.assis.silva@gmail.com>
2018-09-02 12:33:46 -0300

    continuacao da lista de ponteiros

---
+++ ListaPonteiros_MateusDeAssisSilva/questao15.c
@@ -0,0 +1,35 @@
+#include<stdio.h> // scanf, printf
+#include<stdlib.h> //malloc, free, rand, qsort
+
+int compar (const void* p1, const void* p2); // declaração de função de comparação
+
+int main (){
+    float *ptr; // ponteiro pra o array de valores
+    int n, i; // quantos valores? iterador
+    printf("Quantos valores? ");
+    scanf("%d", &n);
+    ptr = (float*) malloc (sizeof(float)*n); // alocação de memoria pro vetor de floats
+    printf("\n Alocacando valores: \n ");
+    for(i = 0; i < n; i++){
+        scanf("%f",&ptr[i]);
+    }
+    printf("\n Eis os valores organizados: \n");
+    qsort(ptr, n, sizeof(float),compar); // aqui indicamos qual vetor queremos organizar,
+                                        // seu tamanho e a função de comparação. A função
+                                        //qsort irá organizar
+    for(i = 0; i < n; i++){
+        printf(" %.1f ", ptr[i]);
+    }
+    free(ptr); // liberação de memoria
+    return 0 ;
+}
+
+int compar (const void* p1, const void* p2){
+    return (*(int*)p1 - *(int*)p2); // o elemento p1 deve vir antes do p2, logo, a diferença tem que ser negativa
+    /*
+    suponha p1 = 10 3 p2 = 30
+    se 10-30< 0, logo p1 vem antes
+    se 30-10>0, p2 vai antes de p1
+
+    */
+}
---
+++ ListaPonteiros_MateusDeAssisSilva/questao18.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void soma(float *p3, float *p1, float *p2, int n){ // uma função que recebe tres inteiros e um inteiro. Sem retorno
+    int i;
+    for(i = 0; i< n; i++){
+        p3[i]= p1[i]+p2[i];
+    }
+}
+
+int main (){
+    void (*ptr_f)(float*, float*,float*, int); //aqui temos um ponteiro para uma função qualquer,
+    // a qual deve receber tres ponteiros pra float e um inteiro
+    ptr_f = &soma; // o ponteiro pra função recebe o endereço do bloco de codigo relacionado a função soma
+    int i,j,n;
+    float *p3; float *p1; float *p2;
+    printf("Quantos termos?\t");
+    scanf("%d", &n);
+    p1 = (float*)malloc(n*sizeof(float));
+    p2 = (float*)malloc(n*sizeof(float));
+    p3 = (float*)malloc(n*sizeof(float));
+    for(j=1;j<=2;j++){
+        if(j==1)
+            printf("Alocacao do primeiro vetor...\n");
+        else
+            printf("Alocacao do segundo vetor...\n");
+        for(i=0;i<n;i++){
+            if(j==1)
+                scanf("%f", &p1[i]);
+            else
+                scanf("%f", &p2[i]);
+        }
+    }
+    (*ptr_f)(p3,p1,p2,n);
+    printf("Vetor resposta:");
+    for(i=0;i<n;i++){
+        printf("%f ", p3[i]);
+    }
+    free(p1);
+    free(p2);
+    free(p3);
+    return 0;
+}
